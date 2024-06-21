4a0f2999a524848c4435629e48fc173910db360b
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-06-21 23:29:45 +0600

    dynamic programming

--- vite-project/src/pages/ToDoApp.jsx
+++ vite-project/src/pages/ToDoApp.jsx
@@ -1,71 +1,329 @@
 import React from 'react';
 
 
-const tasks = {
-    id: "unique-id-001",
-    subtitle: "Subtitle",
-    createdAt: '2024-06-21T06:27:15.501Z',
-    tags: [
-        {
-            id: "unique-id-001",
-            text: "It's Done",
-            icon: "T",
-        },
-        {
-            id: "unique-id-002",
-            text: "It's Cancelled",
-            icon: "C",
-        },
-        {
-            id: "unique-id-003",
-            text: "It's in Progress",
-            icon: "P",
-        },
-        {
-            id: "unique-id-004",
-            text: "Just wrote this",
-            icon: "C",
-        }
-    ],
-    comments: [{
-        id: "comment-id-001",
-        user: {
-            avatar: "xyz.com",
-            name: "Viraj",
-            id: "user-id-001",
-        },
-        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
+const tasks = [
+    {
+        id: "unique-id-001",
+        subtitle: "Subtitle",
+        createdAt: '2024-06-21T06:27:15.501Z',
+        tags: [
+            {
+                id: "unique-id-001",
+                text: "It's Done",
+                icon: "T",
+            },
+            {
+                id: "unique-id-002",
+                text: "It's Cancelled",
+                icon: "C",
+            },
+            {
+                id: "unique-id-003",
+                text: "It's in Progress",
+                icon: "P",
+            },
+            {
+                id: "unique-id-004",
+                text: "Just wrote this",
+                icon: "C",
+            }
+        ],
+        comments: [{
+            id: "comment-id-001",
+            user: {
+                avatar: "xyz.com",
+                name: "Viraj",
+                id: "user-id-001",
+            },
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
 
 
-    }],
-    taskName: [
-        {
-            id: "task-id-001",
-            title:"Foggy Nelson",
-            text: "Here to clean the streets of hells kitchen",
-            status: "done"
-        },
-        {
-            id: "task-id-002",
-            title:"Louis CK",
-            text: "Here to clean the streets of hells kitchen",
-            status: "cancelled"
-        },
-        {
-            id: "task-id-003",
-            title:"Albart Einstain",
-            text: "Here to clean the streets of hells kitchen",
-            status: "progress"
-        },
-        {
-            id: "task-id-004",
-            title:"Albart Hiccup",
-            text: "Here to clean the streets of hells kitchen",
-            status: "progress"
-        }
-    ]
+        }],
+        tasks: [
+            {
+                id: "task-id-001",
+                title:"Foggy Nelson",
+                text: "Here to clean the streets of hells kitchen",
+                status: "done"
+            },
+            {
+                id: "task-id-002",
+                title:"Louis CK",
+                text: "Here to clean the streets of hells kitchen",
+                status: "cancelled"
+            },
+            {
+                id: "task-id-003",
+                title:"Albart Einstain",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            },
+            {
+                id: "task-id-004",
+                title:"Albart Hiccup",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            }
+        ]
 
-}
+    },
+    {
+        id: "unique-id-001",
+        subtitle: "Subtitle",
+        createdAt: '2024-06-21T06:27:15.501Z',
+        tags: [
+            {
+                id: "unique-id-001",
+                text: "It's Done",
+                icon: "T",
+            },
+            {
+                id: "unique-id-002",
+                text: "It's Cancelled",
+                icon: "C",
+            }
+        ],
+        comments: [{
+            id: "comment-id-001",
+            user: {
+                avatar: "xyz.com",
+                name: "Viraj",
+                id: "user-id-001",
+            },
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
+
+
+        }],
+        tasks: [
+            {
+                id: "task-id-001",
+                title:"Foggy Nelson",
+                text: "Here to clean the streets of hells kitchen",
+                status: "done"
+            },
+            {
+                id: "task-id-002",
+                title:"Louis CK",
+                text: "Here to clean the streets of hells kitchen",
+                status: "cancelled"
+            },
+            {
+                id: "task-id-003",
+                title:"Albart Einstain",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            },
+
+        ]
+
+    },
+    {
+        id: "unique-id-001",
+        subtitle: "Subtitle",
+        createdAt: '2024-06-21T06:27:15.501Z',
+        tags: [
+            {
+                id: "unique-id-001",
+                text: "It's Done",
+                icon: "T",
+            },
+            {
+                id: "unique-id-002",
+                text: "It's Cancelled",
+                icon: "C",
+            },
+            {
+                id: "unique-id-003",
+                text: "It's in Progress",
+                icon: "P",
+            },
+            {
+                id: "unique-id-004",
+                text: "Just wrote this",
+                icon: "C",
+            }
+        ],
+        comments: [{
+            id: "comment-id-001",
+            user: {
+                avatar: "xyz.com",
+                name: "Viraj",
+                id: "user-id-001",
+            },
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
+
+
+        }],
+        tasks: [
+
+            {
+                id: "task-id-003",
+                title:"Albart Einstain",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            },
+            {
+                id: "task-id-004",
+                title:"Albart Hiccup",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            }
+        ]
+
+    },
+    {
+        id: "unique-id-001",
+        subtitle: "Subtitle",
+        createdAt: '2024-06-21T06:27:15.501Z',
+        tags: [
+
+            {
+                id: "unique-id-004",
+                text: "Just wrote this",
+                icon: "C",
+            }
+        ],
+        comments: [{
+            id: "comment-id-001",
+            user: {
+                avatar: "xyz.com",
+                name: "Viraj",
+                id: "user-id-001",
+            },
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
+
+
+        }],
+        tasks: [
+            {
+                id: "task-id-001",
+                title:"Foggy Nelson",
+                text: "Here to clean the streets of hells kitchen",
+                status: "done"
+            },
+
+            {
+                id: "task-id-003",
+                title:"Albart Einstain",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            },
+            {
+                id: "task-id-004",
+                title:"Albart Hiccup",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            }
+        ]
+
+    },
+    {
+        id: "unique-id-001",
+        subtitle: "Subtitle",
+        createdAt: '2024-06-21T06:27:15.501Z',
+        tags: [
+            {
+                id: "unique-id-001",
+                text: "It's Done",
+                icon: "T",
+            },
+            {
+                id: "unique-id-002",
+                text: "It's Cancelled",
+                icon: "C",
+            },
+
+            {
+                id: "unique-id-004",
+                text: "Just wrote this",
+                icon: "C",
+            }
+        ],
+        comments: [{
+            id: "comment-id-001",
+            user: {
+                avatar: "xyz.com",
+                name: "Viraj",
+                id: "user-id-001",
+            },
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
+
+
+        }],
+        tasks: [
+
+            {
+                id: "task-id-003",
+                title:"Albart Einstain",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            },
+            {
+                id: "task-id-004",
+                title:"Albart Hiccup",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            }
+        ]
+
+    },
+    {
+        id: "unique-id-001",
+        subtitle: "Subtitle",
+        createdAt: '2024-06-21T06:27:15.501Z',
+        tags: [
+            {
+                id: "unique-id-001",
+                text: "It's Done",
+                icon: "T",
+            },
+            {
+                id: "unique-id-002",
+                text: "It's Cancelled",
+                icon: "C",
+            },
+
+            {
+                id: "unique-id-004",
+                text: "Just wrote this",
+                icon: "C",
+            }
+        ],
+        comments: [{
+            id: "comment-id-001",
+            user: {
+                avatar: "xyz.com",
+                name: "Viraj",
+                id: "user-id-001",
+            },
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequatur cum delectus eius id libero non provident, reiciendis voluptas voluptate."
+
+
+        }],
+        tasks: [
+            {
+                id: "task-id-001",
+                title:"Foggy Nelson",
+                text: "Here to clean the streets of hells kitchen",
+                status: "done"
+            },
+            {
+                id: "task-id-002",
+                title:"Louis CK",
+                text: "Here to clean the streets of hells kitchen",
+                status: "cancelled"
+            },
+
+            {
+                id: "task-id-004",
+                title:"Albart Hiccup",
+                text: "Here to clean the streets of hells kitchen",
+                status: "progress"
+            }
+        ]
+
+    }
+]
 
 function getDay(dateStr){
     const date = new Date(dateStr).getDay();
@@ -89,25 +347,31 @@ const CommentListItem = ({comment})=> {
     return <div key={comment.id}><h3>{comment.user.name} </h3> <p>{comment.text}</p></div>
 }
 const TaskListItem = ({task})=> {
-    return <li key={task.id}><h3>{task.title}</h3> <p><small>{task.status}</small></p> <p>{task.text}</p></li>
+    return (<li key={task.id}><h3>{task.title}</h3> <p><small>{task.status}</small></p> <p>{task.text}</p></li>)
+}
+const TaskCard = ({task})=> {
+    return (<div className="box-card">
+        <h1 className="title">{getDay(task.createdAt)}, {formatDate(task.createdAt)}</h1>
+        <h3 className="sub-title">{task.subtitle}</h3>
+        <ul className="tag-ul">
+            {task.tags.map((tag) => <TagListItem key={tag.id} tag={tag}/>)}
+        </ul>
+        <div className="line"/>
+        <p>Note's Linked to people</p>
+        <div className="comments">
+            {task.comments.map((comment) => <CommentListItem key={comment.id} comment={comment}/>)}
+        </div>
+        <ul className="tasks">
+            {task.tasks.map((task) => <TaskListItem key={task.id} task={task}/>)}
+        </ul>
+    </div>)
 }
 
 const ToDoApp = () => {
     return (
-        <div className="box-card">
-            <h1 className="title">{getDay(tasks.createdAt)}, {formatDate(tasks.createdAt)}</h1>
-            <h3 className="sub-title">{tasks.subtitle}</h3>
-            <ul className="tag-ul">
-                {tasks.tags.map((tag) => <TagListItem key={tag.id} tag={tag}/>)}
-            </ul>
-            <hr/>
-            <p>Note's Linked to people</p>
-            <div>
-                {tasks.comments.map((comment) => <CommentListItem key={comment.id} comment={comment}/>)}
-            </div>
-            <ul>
-                {tasks.taskName.map((task)=> <TaskListItem key={task.id} task={task}/>)}
-            </ul>
+        <div className="task-elements">
+
+            {tasks.map(task => <TaskCard task={task} key={task.id} />)}
         </div>
     );
 };
