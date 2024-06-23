7bcdd0578a499c21219cf0676ccd4c7bb9bcacb8
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-06-23 09:05:47 +0600

    handle input fildes with some ideas

--- vite-project/src/pages/CalculatorApp.jsx
+++ vite-project/src/pages/CalculatorApp.jsx
@@ -1,3 +1,10 @@
+// Handle user input
+// Handle Operations
+// Handle a list of history
+// Render history
+// Restore history
+
+
 import React, {useState} from 'react';
 
 
@@ -9,20 +16,43 @@ const InitialState = {
 const CalculatorApp = () => {
 
   const [inputState, setInputState] = useState({...InitialState});
-  const handleInputForms = (e)=>{
-      setInputState({...inputState,});
-      [e.target.name]: e.target.value;
+  // const handleInputForms = (e)=> {
+  //     setInputState({
+  //      ...inputState,
+  //     [e.target.name]: parseInt(e.target.value),
+  //     });
+  //
+  // }
 
-  }
+  // const handleInputA = (e)=>{
+  //     setInputState({
+  //         ...inputState,
+  //         a: parseInt(e.target.value),
+  //
+  //     })
+  // }
+  //   const handleInputB = (e)=>{
+  //       setInputState({
+  //           ...inputState,
+  //           b: parseInt(e.target.value),
+  //
+  //       })
+  //   }
+    const handleInputChange = (inp)=>{
+        setInputState({
+            ...inputState,
+            ...inp
+        });
+    }
 
 
     return (
         <div style={{ padding: "1rem", width: "50%", margin: "0 auto" }}>
-            <h1>Result : 0</h1>
+            <h1>Result : 0 </h1>
             <div style={{marginTop: "1rem"}}>
                 <h3 style={{marginBottom: "1rem"}}>Inputs :</h3>
-                <input onChange={handleInputForms}  type="number" value={inputState.a}/>
-                <input onChange={handleInputForms} type="number" value={inputState.b}/>
+                <input onChange={(e)=>handleInputChange({a: parseInt(e.target.value)})}  type="number" value={inputState.a} name="a"/>
+                <input onChange={(e)=>handleInputChange({b: parseInt(e.target.value)})} type="number" value={inputState.b} name="b"/>
             </div>
             <div style={{marginTop: "1rem"}}>
                 <h3>Operations :</h3>
